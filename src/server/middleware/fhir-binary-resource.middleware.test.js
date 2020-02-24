9fdf338f89dd7051bff392a03665f34287314408
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-02-24 19:09:36 +0000

    tests(middleware): add basic tests for fhir binary middleware

---
+++ src/server/middleware/fhir-binary-resource.middleware.test.js
@@ -0,0 +1,32 @@
+/* eslint-disable jest/no-commented-out-tests */
+/* eslint-disable jest/expect-expect */
+const httpMocks = require('node-mocks-http');
+const fhirBinaryMiddleware = require('../middleware/fhir-binary-resource.middleware');
+
+const args = {
+	id: '1'
+};
+
+describe('FHIR Binary resource middleware', () => {
+	test('Should return a middleware function', () => {
+		const middleware = fhirBinaryMiddleware();
+		expect(typeof middleware).toBe('function');
+	});
+
+	test('Should pass an error to next if mandatory value is missing', () => {
+		const middleware = fhirBinaryMiddleware();
+
+		const query = {};
+		const req = {
+			method: 'PUT',
+			body: Object.assign(query, args)
+		};
+		const res = httpMocks.createResponse();
+		const next = jest.fn();
+
+		middleware(req, res, next);
+		expect(next.mock.calls[0][0]).toBe('File missing from request');
+		expect(res.statusCode).toBe(400);
+		expect(next).toHaveBeenCalledTimes(1);
+	});
+});
