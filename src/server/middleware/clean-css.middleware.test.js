c12c82009e6419a338878671ec13fa2d0e31a6dc
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-02-28 12:23:03 +0000

    tests(middleware): update tests for cleanCssMiddleware function

--- src/server/middleware/clean-css.middleware.test.js
+++ src/server/middleware/clean-css.middleware.test.js
@@ -8,11 +8,33 @@ describe('Clean CSS middleware', () => {
 		expect(typeof middleware).toBe('function');
 	});
 
-	test('Should clean CSS', async () => {
-		const middleware = cleanCssMiddleware('arial, sans-serif', 10);
+	test('Should clean CSS and change font', async () => {
+		const middleware = cleanCssMiddleware();
 		const req = {
 			body: fs.readFileSync(
 				'./test_files/tester_bullet_issues-html.html',
+				{ encoding: 'UTF-8' }
+			),
+			query: {
+				fonts: 'arial'
+			},
+			results: {}
+		};
+		const res = httpMocks.createResponse();
+		const next = jest.fn();
+
+		await middleware(req, res, next);
+		expect(typeof req.results).toBe('object');
+		expect(req.results.clean_css).toBe('Fixed');
+		expect(next).toHaveBeenCalledTimes(1);
+		expect(/font-family: arial;/gm.exec(req.body)).not.toBeNull();
+	});
+
+	test('Should flag file as passed if no issues found', async () => {
+		const middleware = cleanCssMiddleware();
+		const req = {
+			body: fs.readFileSync(
+				'./test_files/empty-test.html',
 				{ encoding: 'UTF-8' }
 			),
 			results: {}
@@ -21,13 +43,13 @@ describe('Clean CSS middleware', () => {
 		const next = jest.fn();
 
 		await middleware(req, res, next);
-		expect(req.results.clean_css).toBe('Fixed');
+		expect(typeof req.results).toBe('object');
+		expect(req.results.clean_css).toBe('Passed');
 		expect(next).toHaveBeenCalledTimes(1);
-		expect(/font-size:\s*([0-9])px;/gm.exec(req.body)).toBeNull();
 	});
 
 	test('Should build req.results if not defined', async () => {
-		const middleware = cleanCssMiddleware(10, 'arial, sans-serif');
+		const middleware = cleanCssMiddleware();
 		const req = {
 			body: fs.readFileSync(
 				'./test_files/tester_bullet_issues-html.html',
