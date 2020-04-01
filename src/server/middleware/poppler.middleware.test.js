f16e127f3ec17485c096bc716238f5351c29f826
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-04-01 16:04:33 +0100

    tests(middleware): add check for directory creation

--- src/server/middleware/poppler.middleware.test.js
+++ src/server/middleware/poppler.middleware.test.js
@@ -29,10 +29,12 @@ describe('Poppler conversion middleware', () => {
 	});
 
 	test('Should convert PDF to HTML and place in specified directory', async () => {
-		const middleware = popplerMiddleware({
+		const options = {
 			tempDirectory: './src/server/temp/',
 			encoding: 'UTF-8'
-		});
+		};
+
+		const middleware = popplerMiddleware(options);
 
 		const req = {
 			body: fs.readFileSync('./test_files/pdf_1.3_NHS_Constitution.pdf', {
@@ -47,6 +49,7 @@ describe('Poppler conversion middleware', () => {
 		expect(typeof req.body).toBe('string');
 		expect(typeof req.doclocation).toBe('object');
 		expect(fs.existsSync(req.doclocation.html)).toBe(true);
+		expect(fs.existsSync(options.tempDirectory)).toBe(true);
 		expect(next).toHaveBeenCalledTimes(1);
 	});
 });
