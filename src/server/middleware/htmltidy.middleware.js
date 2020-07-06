6166a48246af2b2885ecdd2b3d40ebf780932dac
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-07-06 08:01:04 +0100

    refactor(middleware): tidy function structure

--- src/server/middleware/htmltidy.middleware.js
+++ src/server/middleware/htmltidy.middleware.js
@@ -1,4 +1,24 @@
+const isHtml = require('is-html');
 const { tidy } = require('htmltidy2');
+
+/**
+ * @author Frazer Smith
+ * @description Use HTMLTidy2 to parse and tidy HTML passed.
+ * @param {string} html - HTML string.
+ * @param {string} config - HTMLTidy2 configuration values.
+ * @returns {Promise<string>} Promise of parsed HTML string on resolve, or error string on rejection.
+ */
+function tidyHtml(html, config) {
+	return new Promise((resolve, reject) => {
+		// eslint-disable-next-line promise/prefer-await-to-callbacks
+		tidy(html, config, (err, tidiedHtml) => {
+			if (err) {
+				reject(err);
+			}
+			resolve(tidiedHtml);
+		});
+	});
+}
 
 /**
  * @author Frazer Smith
@@ -7,12 +27,13 @@ const { tidy } = require('htmltidy2');
  * @returns {Function} Express middleware.
  */
 module.exports = function htmltidyMiddleware(config = {}) {
-	return async (req, res, next) =>
-		new Promise((resolve) => {
-			// eslint-disable-next-line promise/prefer-await-to-callbacks
-			tidy(req.body, config, (err, tidiedHtml) => {
-				req.body = tidiedHtml;
-				resolve(next());
-			});
-		});
+	return async (req, res, next) => {
+		if (typeof req.body === 'string' && isHtml(req.body)) {
+			req.body = await tidyHtml(req.body, config);
+			next();
+		} else {
+			res.status(400);
+			next(new Error('Invalid HTML passed to htmltidy middleware'));
+		}
+	};
 };
