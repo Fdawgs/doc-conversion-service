887ce23026fc72ee2e418740e36965d6580cce4e
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-07-07 12:38:11 +0100

    refactor(middleware): use util.promisify for htmltidy2 function

--- src/server/middleware/htmltidy.middleware.js
+++ src/server/middleware/htmltidy.middleware.js
@@ -1,21 +1,8 @@
 const isHtml = require('is-html');
 const { tidy } = require('htmltidy2');
+const util = require('util');
 
-/**
- * @author Frazer Smith
- * @description Use HTMLTidy2 to parse and tidy HTML passed.
- * @param {string} html - HTML string.
- * @param {string} config - HTMLTidy2 configuration values.
- * @returns {Promise<string>} Promise of parsed HTML string on resolve, or error string on rejection.
- */
-function tidyHtml(html, config) {
-	return new Promise((resolve) => {
-		// eslint-disable-next-line promise/prefer-await-to-callbacks
-		tidy(html, config, (err, tidiedHtml) => {
-			resolve(tidiedHtml);
-		});
-	});
-}
+const tidyHtml = util.promisify(tidy);
 
 /**
  * @author Frazer Smith
