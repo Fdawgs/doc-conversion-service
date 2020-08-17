77449f0464c348ebb0d91cf11019888a69aaecaf
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-08-17 19:19:55 +0100

    chore(tests): correct file remover utility test description

--- src/server/utils/file-remover.utils.test.js
+++ src/server/utils/file-remover.utils.test.js
@@ -2,7 +2,7 @@ const faker = require('faker');
 const fs = require('fs');
 const Util = require('./file-remover.utils');
 
-describe('Error handler utility', () => {
+describe('File remover utility', () => {
 	test('Should remove test file from path', () => {
 		const testPath = `./${faker.system.fileName('txt')}`;
 		fs.writeFileSync(testPath, faker.lorem.sentences());
