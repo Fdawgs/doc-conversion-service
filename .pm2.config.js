dd49dc0265c2462b05a093050e0ae3c5ad82e63d
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-09-08 12:35:03 +0100

    chore(pm2): increase instances from 8 to 16

--- .pm2.config.js
+++ .pm2.config.js
@@ -7,7 +7,7 @@ module.exports = {
 				NODE_ENV: 'production'
 			},
 			exec_mode: 'cluster',
-			instances: 8,
+			instances: 16,
 			name: 'doc-conv-api',
 			script: './src/index.js',
 			watch: ['./src/config.js', '.env.production']
